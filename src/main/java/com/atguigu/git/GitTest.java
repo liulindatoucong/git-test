33a29bfccb0c1dfa668f61966f38dfc7a307a84e
liulin <liulindatoucong@163.com>
2023-02-17 11:40:21 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("Hello git3!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test!");
+
     }
 }
